cdb34b38886d921bc47be590d8b08762466c8f19
zjpan <zjpan123@gmail.com>
2016-03-11 04:20:34 +0800

    Added files via upload

--- task2.py
+++ task2.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import scipy.linalg as sci
 
-img=mpimg.imread('Lenna.tiff')
+#New Picture
+img=mpimg.imread('Reset.jpg')
 [r,g,b] = [img[:,:,i] for i in range(3)]
 
 
@@ -17,3 +19,49 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+[r2,g2,b2] = [img[:,:,i] for i in range(3)]
+
+red = {'Index' : 0,'Mat': r2, 'Color' : 'Reds'}
+green = {'Index' : 1, 'Mat': g2, 'Color' : 'Greens'}
+blue = {'Index' : 2, 'Mat': b2, 'Color' : 'Blues'}
+rgb = [red,green,blue]
+
+for color in rgb:
+    plt.imshow(color['Mat'], cmap = color['Color'])
+    plt.show()
+    
+    # SVD decomposition of img
+    U,s,V = sci.svd(color['Mat'])
+    
+    S = np.zeros(color['Mat'].shape,s.dtype)
+    
+    for i in range(s.size):
+        S[i][i] = s[i]
+        
+    #check: Get back A
+    #check = np.dot(np.dot(U,S),V)
+    #plt.imshow(check, cmap ='Reds')
+    #plt.show()
+    color['U'] = U
+    color['eigenval'] = s
+    color['S'] = S
+    color['V'] = V
+    
+img_new = np.zeros_like(img)
+
+for dimension in (30,200):
+    for color in rgb:
+        S_new = np.zeros(color['Mat'].shape,s.dtype)
+        
+        for i in range(dimension):
+            S_new[i][i] = color['eigenval'][i]
+            
+        img_new[:,:,color['Index']]= np.dot(np.dot(color['U'],S_new),color['V'])
+    plt.imshow(img_new)
+    #Create New Image with Lower Resolution
+    if (dimension == 30 ):
+        plt.imsave('Reset_lower.jpg',img_new)
+    #Create New Image with Better Resolution
+    if (dimension == 200):
+        plt.imsave('Reset_better.jpg',img_new)
+    plt.show()
